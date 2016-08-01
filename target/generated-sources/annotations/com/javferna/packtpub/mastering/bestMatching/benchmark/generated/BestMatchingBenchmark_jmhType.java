eb4eff06d2c790bc5d9816cd10081900ea4c99f0
huangzhi29 <huangzhi29@vip.qq.com>
2016-08-01 14:41:28 +0800

    first

---
+++ target/generated-sources/annotations/com/javferna/packtpub/mastering/bestMatching/benchmark/generated/BestMatchingBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package com.javferna.packtpub.mastering.bestMatching.benchmark.generated;
+public class BestMatchingBenchmark_jmhType extends BestMatchingBenchmark_jmhType_B3 {
+}
+
---
+++ target/generated-sources/annotations/com/javferna/packtpub/mastering/bestMatching/benchmark/generated/ExistsBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package com.javferna.packtpub.mastering.bestMatching.benchmark.generated;
+public class ExistsBenchmark_jmhType extends ExistsBenchmark_jmhType_B3 {
+}
+
